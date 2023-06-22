d4e52a7837982acb1ce213e11de74b9cfb7e26cb
need <need@needix.de>
2023-06-22 22:03:34 +0200

    Corrected inital piece placement

--- src/main/java/de/needix/code/model/Board.java
+++ src/main/java/de/needix/code/model/Board.java
@@ -20,31 +20,41 @@ public class Board {
     private void fillBoard() {
         pieces = new Piece[MainController.BOARD_SIZE][MainController.BOARD_SIZE];
         pieces[0][0] = new RookPiece().setTeam(Team.WHITE);
-        pieces[7][0] = new RookPiece().setTeam(Team.WHITE);
-        pieces[0][7] = new RookPiece().setTeam(Team.BLACK);
+        pieces[0][7] = new RookPiece().setTeam(Team.WHITE);
+        pieces[7][0] = new RookPiece().setTeam(Team.BLACK);
         pieces[7][7] = new RookPiece().setTeam(Team.BLACK);
 
-        pieces[0][1] = new KnightPiece().setTeam(Team.WHITE);
-        pieces[0][6] = new KnightPiece().setTeam(Team.WHITE);
-        pieces[7][1] = new KnightPiece().setTeam(Team.BLACK);
-        pieces[7][6] = new KnightPiece().setTeam(Team.BLACK);
+        pieces[1][0] = new KnightPiece().setTeam(Team.WHITE);
+        pieces[6][0] = new KnightPiece().setTeam(Team.WHITE);
+        pieces[1][7] = new KnightPiece().setTeam(Team.BLACK);
+        pieces[6][7] = new KnightPiece().setTeam(Team.BLACK);
 
-        pieces[0][2] = new BishopPiece().setTeam(Team.WHITE);
-        pieces[0][5] = new BishopPiece().setTeam(Team.WHITE);
-        pieces[7][2] = new BishopPiece().setTeam(Team.BLACK);
-        pieces[7][5] = new BishopPiece().setTeam(Team.BLACK);
+        pieces[2][0] = new BishopPiece().setTeam(Team.WHITE);
+        pieces[5][0] = new BishopPiece().setTeam(Team.WHITE);
+        pieces[2][7] = new BishopPiece().setTeam(Team.BLACK);
+        pieces[5][7] = new BishopPiece().setTeam(Team.BLACK);
 
-        pieces[0][3] = new QueenPiece().setTeam(Team.WHITE);
-        pieces[7][3] = new QueenPiece().setTeam(Team.BLACK);
+        pieces[3][0] = new QueenPiece().setTeam(Team.WHITE);
+        pieces[3][7] = new QueenPiece().setTeam(Team.BLACK);
 
-        pieces[0][4] = new KingPiece().setTeam(Team.WHITE);
-        pieces[7][4] = new KingPiece().setTeam(Team.BLACK);
+        pieces[4][0] = new KingPiece().setTeam(Team.WHITE);
+        pieces[4][7] = new KingPiece().setTeam(Team.BLACK);
 
         for (int i = 0; i < MainController.BOARD_SIZE; i++) {
-            pieces[1][i] = new PawnPiece().setTeam(Team.WHITE);
-            pieces[6][i] = new PawnPiece().setTeam(Team.BLACK);
+            pieces[i][1] = new PawnPiece().setTeam(Team.WHITE);
+            pieces[i][6] = new PawnPiece().setTeam(Team.BLACK);
+        }
+
+        for (int y = 0; y < MainController.BOARD_SIZE; y++) {
+            for (int x = 0; x < MainController.BOARD_SIZE; x++) {
+                if (getPiece(x, y) == null) {
+                    pieces[x][y] = new EmptyPiece();
+                }
+            }
         }
     }
 
-    public void getPiece(int x, int y) {}
+    public Piece getPiece(int x, int y) {
+        return pieces[x][y];
+    }
 }
