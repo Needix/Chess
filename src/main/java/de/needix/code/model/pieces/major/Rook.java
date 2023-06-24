8c6108db6c2d36bce04e68a6eb20c58b4b4bc1e5
Konstantin <k.todor@outlook.de>
2023-06-24 12:58:53 +0200

    Added validMoves for Rook

--- src/main/java/de/needix/code/model/pieces/major/Rook.java
+++ src/main/java/de/needix/code/model/pieces/major/Rook.java
@@ -1,8 +1,10 @@
 package de.needix.code.model.pieces.major;
 
 import java.awt.Point;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+
+import de.needix.code.controller.MainController;
 import de.needix.code.model.Board;
 import de.needix.code.model.pieces.Piece;
 
@@ -11,12 +13,72 @@ public class Rook extends Piece {
 
     @Override
     public String getDisplayCharacter() {
-        return "Rook";
+        switch (getTeam()) {
+            case WHITE:
+                return "♖";
+            case BLACK:
+                return "♜";
+        
+            default:
+                return "";
+        }
     }
 
     @Override
     public List<Point> getValidMoves(Board currentBoard, Point currentPosition) {
-        return Collections.emptyList();
+        List<Point> validMoves = new ArrayList<>();
+
+        //X-axis - right
+        for (int i = currentPosition.x; i < MainController.BOARD_SIZE; i++) {
+            Point check = new Point(i, currentPosition.y);
+            if(currentBoard.isInBounds(currentPosition)){
+                Piece blockingPiece = currentBoard.getPiece(currentPosition);
+                if(!blockingPiece.isValidPiece()){
+                    validMoves.add(check);
+                } else{
+                    break;
+                }
+            }
+        }
+        //X-axis - left
+        for (int i = currentPosition.x; i >= 0; i--) {
+            Point check = new Point(i, currentPosition.y);
+            if(currentBoard.isInBounds(currentPosition)){
+                Piece blockingPiece = currentBoard.getPiece(currentPosition);
+                if(!blockingPiece.isValidPiece()){
+                    validMoves.add(check);
+                } else{
+                    break;
+                }
+            }
+        }
+
+        //Y-axis - up
+        for (int i = currentPosition.x; i < MainController.BOARD_SIZE; i++) {
+            Point check = new Point(currentPosition.x, i);
+            if(currentBoard.isInBounds(currentPosition)){
+                Piece blockingPiece = currentBoard.getPiece(currentPosition);
+                if(!blockingPiece.isValidPiece()){
+                    validMoves.add(check);
+                } else{
+                    break;
+                }
+            }
+        }
+        //Y-axis - down
+        for (int i = currentPosition.x; i >= 0; i--) {
+            Point check = new Point(currentPosition.x, i);
+            if(currentBoard.isInBounds(currentPosition)){
+                Piece blockingPiece = currentBoard.getPiece(currentPosition);
+                if(!blockingPiece.isValidPiece()){
+                    validMoves.add(check);
+                } else{
+                    break;
+                }
+            }
+        }
+
+        return validMoves;
     }
 
 }
--- src/main/java/de/needix/code/model/pieces/minor/Pawn.java
+++ src/main/java/de/needix/code/model/pieces/minor/Pawn.java
@@ -2,8 +2,6 @@ package de.needix.code.model.pieces.minor;
 
 import java.awt.Point;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import de.needix.code.controller.MainController;
