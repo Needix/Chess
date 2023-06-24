c85ababd08b83fd8ad03a4e5e796fbbefbfa11d3
Konstantin <k.todor@outlook.de>
2023-06-24 15:31:03 +0200

    Added validMoves for Queen

--- src/main/java/de/needix/code/model/pieces/major/Queen.java
+++ src/main/java/de/needix/code/model/pieces/major/Queen.java
@@ -1,6 +1,7 @@
 package de.needix.code.model.pieces.major;
 
 import java.awt.Point;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import de.needix.code.model.Board;
@@ -23,6 +24,19 @@ public class Queen extends Piece {
 
     @Override
     public List<Point> getValidMoves(Board currentBoard, Point currentPosition) {
-        return Collections.emptyList();
+        List<Point> validMoves = new ArrayList<>();
+        
+        Rook rook = new Rook();
+        rook.setTeam(this.getTeam());
+        Bishop bishop = new Bishop();
+        bishop.setTeam(this.getTeam());
+
+        List<Point> rookMoves = rook.getValidMoves(currentBoard, currentPosition);
+        List<Point> bishopMoves = bishop.getValidMoves(currentBoard, currentPosition);
+
+        validMoves.addAll(rookMoves);
+        validMoves.addAll(bishopMoves);
+
+        return validMoves;
     }
 }
